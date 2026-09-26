f124cfa56ac3d5d005d7c75c9364db89f45d9240
josepha_mayo <192471872+josepha-mayo@users.noreply.github.com>
2026-09-26 02:43:02 +0100

    Control: remove old-update synchronization only from SQL source

--- ci/warehouse-migration.sql
+++ ci/warehouse-migration.sql
@@ -1,8 +1,4 @@
 ALTER TABLE stock_items ADD COLUMN fulfillment_bin TEXT;
-CREATE TRIGGER sync_pick_to_fulfillment AFTER UPDATE OF pick_bin ON stock_items
-BEGIN
-  UPDATE stock_items SET fulfillment_bin = NEW.pick_bin WHERE id = NEW.id;
-END;
 CREATE TRIGGER sync_insert_to_fulfillment AFTER INSERT ON stock_items
 WHEN NEW.fulfillment_bin IS NULL
 BEGIN
