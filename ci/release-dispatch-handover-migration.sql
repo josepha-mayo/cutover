55669c7e0f8c71b61f797189e5af559b29f9b1d1
josepha_mayo <192471872+josepha-mayo@users.noreply.github.com>
2026-09-26 03:46:12 +0100

    Control: remove only the downloaded migration's old-write bridge

--- ci/release-dispatch-handover-migration.sql
+++ ci/release-dispatch-handover-migration.sql
@@ -1,9 +1,4 @@
 ALTER TABLE "consignments" ADD COLUMN "dispatch_bay" TEXT;
-CREATE TRIGGER cutover_old_update AFTER UPDATE OF "loading_bay" ON "consignments"
-WHEN NEW."loading_bay" IS NOT NEW."dispatch_bay"
-BEGIN
-  UPDATE "consignments" SET "dispatch_bay" = NEW."loading_bay" WHERE id = NEW.id;
-END;
 CREATE TRIGGER cutover_new_update AFTER UPDATE OF "dispatch_bay" ON "consignments"
 WHEN NEW."dispatch_bay" IS NOT NEW."loading_bay"
 BEGIN
