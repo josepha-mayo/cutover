a2e3cedcea3a70be3502034467c8bb947edb0fa2
josepha_mayo <192471872+josepha-mayo@users.noreply.github.com>
2026-09-26 03:41:25 +0100

    Control: install the exact browser-generated Action workflow

---
+++ ci/release-dispatch-handover-migration.sql
@@ -0,0 +1,17 @@
+ALTER TABLE "consignments" ADD COLUMN "dispatch_bay" TEXT;
+CREATE TRIGGER cutover_old_update AFTER UPDATE OF "loading_bay" ON "consignments"
+WHEN NEW."loading_bay" IS NOT NEW."dispatch_bay"
+BEGIN
+  UPDATE "consignments" SET "dispatch_bay" = NEW."loading_bay" WHERE id = NEW.id;
+END;
+CREATE TRIGGER cutover_new_update AFTER UPDATE OF "dispatch_bay" ON "consignments"
+WHEN NEW."dispatch_bay" IS NOT NEW."loading_bay"
+BEGIN
+  UPDATE "consignments" SET "loading_bay" = NEW."dispatch_bay" WHERE id = NEW.id;
+END;
+CREATE TRIGGER cutover_old_insert AFTER INSERT ON "consignments"
+WHEN NEW."dispatch_bay" IS NULL
+BEGIN
+  UPDATE "consignments" SET "dispatch_bay" = NEW."loading_bay" WHERE id = NEW.id;
+END;
+UPDATE "consignments" SET "dispatch_bay" = "loading_bay";
